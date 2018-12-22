a3c224df48bcf2f500ae6e2005b781475697ac73
Donekulda <mikulda@hotmail.cz>
2018-12-23 00:01:04 +0100

    update

--- Source/Catch_the_Santa/SantaAICharacter.h
+++ Source/Catch_the_Santa/SantaAICharacter.h
@@ -29,6 +29,8 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
-	
+	bool bIsCatched;
+
+	void looseHealth();
 	
 };
