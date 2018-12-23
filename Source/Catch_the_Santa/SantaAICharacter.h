d52f5ecd59d7045a04ccb2ebea6a815a373961e2
Donekulda <mikulda@hotmail.cz>
2018-12-23 21:03:55 +0100

    update

--- Source/Catch_the_Santa/SantaAICharacter.h
+++ Source/Catch_the_Santa/SantaAICharacter.h
@@ -34,6 +34,7 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	UFUNCTION(BlueprintCallable)
 	void looseHealth();
 
 	int GetHealth() { return Health; }
